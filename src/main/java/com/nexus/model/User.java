8925a88bc14a705bf7ddea5e841c8af9038f3740
Luiz Domingues <luiz.f.l.domingues@gmail.com>
2026-03-19 14:45:42 -0300

    fix: unix endline format

--- src/main/java/com/nexus/model/User.java
+++ src/main/java/com/nexus/model/User.java
@@ -1,13 +1,31 @@
 package com.nexus.model;
 
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.nexus.service.Workspace;
+
 public class User {
     private final String username;
     private final String email;
+    private static final String EMAIL_REQUIREMENTS = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    private Workspace workspace;
 
-    public User(String username, String email) {
+    public static boolean isValidEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_REQUIREMENTS);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public User(String username, String email, Workspace workspace) {
         if (username == null || username.isBlank()) {
             throw new IllegalArgumentException("Username não pode ser vazio.");
         }
+        if (!isValidEmail(email)) {
+            throw new IllegalArgumentException("Email invalido.");
+        }
+        this.workspace = workspace;
         this.username = username;
         this.email = email;
     }
@@ -21,6 +39,10 @@ public class User {
     }
 
     public long calculateWorkload() {
-        return 0; 
+        List<Task> userTasks = workspace.getTasks();
+        return userTasks.stream()
+                .filter(task -> task.getOwner() != null && task.getOwner().consultUsername().equals(username))
+                .filter(task -> task.getStatus() != null && task.getStatus().equals(TaskStatus.IN_PROGRESS))
+                .count();
     }
 }
