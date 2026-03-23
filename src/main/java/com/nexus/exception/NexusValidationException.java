7b33c3ba90aada14280cabac3c5e5e7c5272e6ce
luiz domingues <luiz.f.l.domingues@gmail.com>
2026-03-23 18:02:52 -0300

    chore: remove comments from Validation Exception

--- src/main/java/com/nexus/exception/NexusValidationException.java
+++ src/main/java/com/nexus/exception/NexusValidationException.java
@@ -5,8 +5,6 @@ public class NexusValidationException extends RuntimeException {
 
     public NexusValidationException(String message) {
         super(message);
-        // Dica para o aluno: Incrementar contador global de erros aqui? 
-        // Ou melhor deixar para a Task gerenciar.
         totalValidationErrors++;
     }
 
